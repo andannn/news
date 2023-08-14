367091ac3a7d2875f3d2e830de36747d1d62779c
andannn <jqn296763005@gmail.com>
2023-08-14 21:50:34 +0800

    config app icon

---
+++ lib/app/navigation/top_level_navigation.dart
@@ -0,0 +1,13 @@
+//
+// enum TopLevelNavigation {
+//
+//   const TopLevelNavigation({
+//     required this.displayText,
+//     required this.description,
+//   });
+//
+//   final String selectedIcon;
+//   final String unselectedIcon;
+//   final String iconTextId;
+//   final String titleTextId;
+// }
