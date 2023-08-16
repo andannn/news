f647c311cb3b64af04a80a4a9447fc38642d72bf
andannn <jqn296763005@gmail.com>
2023-08-16 08:01:47 +0800

    remove get

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'package:news/core/shared_preference/user_data.dart';
 
 void main() async {
@@ -17,7 +16,7 @@ class NiaApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
+    return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.black26),
