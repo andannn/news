3b7d6659429a82af3c5ff21f3bf12287900041cc
Jiangqn <qingnan.jiang@dhc.com.cn>
2023-08-19 13:36:49 +0800

    bloc step 3

---
+++ test/feature/for_you/test_for_you_bloc.dart
@@ -0,0 +1,61 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:news/core/data/model/followable_topic.dart';
+import 'package:news/core/data/model/topic.dart';
+import 'package:news/core/data/repository/user_data_repository.dart';
+import 'package:news/core/shared_preference/user_data.dart';
+import 'package:news/core/usecase/get_followable_topics_use_case.dart';
+import 'package:news/feature/for_you/bloc/for_you_bloc.dart';
+import 'package:news/feature/for_you/bloc/for_you_ui_state.dart';
+import 'package:news/feature/for_you/bloc/onboarding_ui_state.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../../fake_models/test_topic_repository.dart';
+
+void main() {
+  group('for_you_bloc_test', () {
+    late ForYouBloc forYouBloc;
+
+    late UserDataRepository userDataRepository;
+    late GetFollowableTopicsUseCase getFollowableTopicsUseCase;
+    late TestTopicRepository topicsRepository;
+
+    List<Topic> sampleTopics = [
+      Topic(id: '0', name: '000', shortDescription: "test 0"),
+      Topic(id: '1', name: '111', shortDescription: "test 1"),
+      Topic(id: '2', name: '222', shortDescription: "test 2"),
+    ];
+
+    setUp(() async {
+      TestWidgetsFlutterBinding.ensureInitialized();
+      SharedPreferences.setMockInitialValues({});
+      await niaUserDataSource.init();
+
+      topicsRepository = TestTopicRepository();
+      userDataRepository = OfflineFirstUserDataRepository(niaUserDataSource);
+      getFollowableTopicsUseCase =
+          GetFollowableTopicsUseCase(topicsRepository, userDataRepository);
+
+      forYouBloc = ForYouBloc(
+          userDataRepository: userDataRepository,
+          getFollowableTopicsUseCase: getFollowableTopicsUseCase);
+    });
+
+    test('initial_state_test', () async {
+      expect(forYouBloc.state, equals(ForYouUiState(Loading())));
+    });
+    test('onboarding_state_test', () async {
+      topicsRepository.sendTopics(sampleTopics);
+      await userDataRepository.setFollowedTopicIds({'1'});
+
+      await Future.delayed(const Duration(seconds: 1));
+
+      expect(
+          forYouBloc.state,
+          equals(ForYouUiState(Shown([
+            FollowableTopic(sampleTopics[0], false),
+            FollowableTopic(sampleTopics[1], true),
+            FollowableTopic(sampleTopics[2], false),
+          ]))));
+    });
+  });
+}
