447557b02aa79577ad0fdfb66ddedb91c6b17667
andannn <jqn296763005@gmail.com>
2023-08-22 23:46:18 +0800

    Cached network image

--- lib/feature/for_you/presentation/widget/news_feeditem_widget.dart
+++ lib/feature/for_you/presentation/widget/news_feeditem_widget.dart
@@ -1,4 +1,6 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:news/core/data/model/news_recsource.dart';
 
 class NewsFeedItemWidget extends StatelessWidget {
@@ -10,26 +12,48 @@ class NewsFeedItemWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
       key: ValueKey(newsResource.id),
+      clipBehavior: Clip.hardEdge,
       margin: const EdgeInsets.all(16),
       child: Column(
         children: [
-          SizedBox(height: 16),
+          _buildNetWorkImageResource(context, newsResource.headerImageUrl),
+          const SizedBox(height: 8),
           Padding(
-              padding: EdgeInsets.all(32),
-              child: Column(
-                children: [
-                  SizedBox(height: 16),
-                  Row(children: [
-                    Expanded(flex: 4, child: Text(newsResource.title)),
-                    Expanded(flex: 1, child: SizedBox()),
-                    IconButton(onPressed: null, icon: Icon(Icons.bookmark_add)),
-                  ]),
-                  SizedBox(height: 16),
-                ]
-              )
-          )
+              padding: const EdgeInsets.all(24),
+              child: Column(children: [
+                const SizedBox(height: 16),
+                Row(children: [
+                  Expanded(flex: 5, child: Text(newsResource.title)),
+                  const Expanded(flex: 1, child: SizedBox()),
+                  IconButton(onPressed: null, icon: Icon(Icons.bookmark_add)),
+                ]),
+                const SizedBox(height: 16),
+                Text(newsResource.content),
+                const SizedBox(height: 16),
+              ]))
         ],
       ),
     );
+  }
+}
+
+Widget _buildNetWorkImageResource(
+    BuildContext context, String? headerImageUrl) {
+  if (headerImageUrl == null || headerImageUrl.isEmpty) {
+    return const SizedBox();
+  } else {
+    Widget getInitialWidget(BuildContext context) => SvgPicture.asset(
+          'assets/image/ic_placeholder_default.xml',
+          height: 180,
+          width: double.infinity,
+        );
+
+    return CachedNetworkImage(
+        height: 180,
+        width: double.infinity,
+        placeholder: (context, _) => getInitialWidget(context),
+        fit: BoxFit.cover,
+        imageUrl: headerImageUrl,
+        errorWidget: (context, _, e) => getInitialWidget(context));
   }
 }
